963d883f76fb3848fe28c4cab3ae2e75234afbf6
Mark Ambrocio <wowmarkambro@gmail.com>
2023-05-05 23:07:17 -0600

    add sus

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Blog() {
-	const posts = await fetch(`${process.env.NEXTAUTH_URL!}/api/content`).then(
-		(res) => res.json()
-	);
+	const posts = await fetch(`${process.env.NEXTAUTH_URL!}/api/content`).then((res) => res.json());
 	return (
 		<div>
 			<h1>Welcome to our Blog</h1>
-			<ul>
-				{posts.map((post: any) => (
-					<li key={post.slug}>
-						<Link href={`/blog/${post.slug}`}>{post.title}</Link>
-					</li>
-				))}
-			</ul>
+			<Suspense fallback={<div>loading...</div>}>
+				<ul>
+					{posts.map((post: any) => (
+						<li key={post.slug}>
+							<Link href={`/blog/${post.slug}`}>{post.title}</Link>
+						</li>
+					))}
+				</ul>
+			</Suspense>
 		</div>
 	);
 }

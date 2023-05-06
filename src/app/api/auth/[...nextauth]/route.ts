4e343aa85e204556872344a92bb8464cabdb9815
Mark Ambrocio <wowmarkambro@gmail.com>
2023-05-05 21:05:58 -0600

    fix export issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { prisma } from "~/lib/prisma";
 
-export const authOptions = {
+const authOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		GithubProvider({
--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,24 +1,31 @@
 import { getServerSession } from "next-auth";
 import { prisma } from "~/lib/prisma";
 import FollowClient from "./FollowClient";
-import { authOptions } from "../app/api/auth/[...nextauth]/route";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
 
 interface Props {
 	targetUserId: string;
 }
 
 export default async function FollowButton({ targetUserId }: Props) {
-	const session = await getServerSession(authOptions);
+	const session = await getServerSession({
+		adapter: PrismaAdapter(prisma),
+		providers: [
+			GithubProvider({
+				clientId: process.env.GITHUB_ID!,
+				clientSecret: process.env.GITHUB_SECRET!,
+			}),
+		],
+	});
 
 	const currentUserId = await prisma.user
 		.findFirst({ where: { email: session?.user?.email! } })
 		.then((user) => user?.id!);
 
-	const isFollowing = await prisma.follows.findFirst({
-		where: { followerId: currentUserId, followingId: targetUserId },
-	});
+	// const isFollowing = await prisma.follows.findFirst({
+	// 	where: { followerId: currentUserId, followingId: targetUserId },
+	// });
 
-	return (
-		<FollowClient targetUserId={targetUserId} isFollowing={!!isFollowing} />
-	);
+	return <FollowClient targetUserId={targetUserId} isFollowing={false} />;
 }
